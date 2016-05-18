39c8fd9f9f63ea3e047d8cc6bc653d79cb9423f0
Sergio <sergiomattei@outlook.com>
2016-05-18 16:10:10 -0400

    Fix missing comma in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	author_email='dev@mxio.us',
 	description="A wrapper for iTunes Search API",
 	long_description=open('README.md').read(),
-	keywords='itunes search api wrapper simple clear cover album art music'
+	keywords='itunes search api wrapper simple clear cover album art music',
 	# Package routines
 	packages=find_packages(exclude=['scripts', 'tests']),
 	install_requires=['requests'],
